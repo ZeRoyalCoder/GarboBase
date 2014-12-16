36718c4d783b2a1b66d4f9f4f5901c9ca52deccd
ZeRoyalCoder <bernhardkiv@hotmail.com>
2014-12-16 22:58:42 +0000

    First commit, and added the base class.

---
+++ me/newgarbo/garbobase/GarboBase.java
@@ -0,0 +1,17 @@
+package me.newgarbo.garbobase;
+
+/**
+ * Main class for the GarboBase, extend this class or just use it as a global
+ * API for accessing all the managers and so on. <p> If you do not extend it,
+ * then accessing it by simply calling the static instance which is:
+ * {@code GarboBase.getInstance()} <p> Or, if you really want to, you can use
+ * reflection (you bad boy) and search for private static instance which is:
+ * {@code GarboBase.instance}
+ * 
+ * @author NewGarbo
+ * @version 1.0
+ */
+public class GarboBase
+{
+	
+}
