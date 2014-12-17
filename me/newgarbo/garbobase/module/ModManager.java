5dd840661e1f110e727cd45836b61ba5d4469ab4
ZeRoyalCoder <bernhardkiv@hotmail.com>
2014-12-17 21:57:46 +0000

    Added some documentation

--- me/newgarbo/garbobase/module/ModManager.java
+++ me/newgarbo/garbobase/module/ModManager.java
@@ -2,22 +2,68 @@ package me.newgarbo.garbobase.module;
 
 import java.util.ArrayList;
 
+import net.minecraft.client.model.ModelArmorStand;
 import me.newgarbo.garbobase.module.mods.Module;
 
 public class ModManager
 {
-	public ArrayList<Module> mods;
+	/**
+	 * A list of all of the Modules that are registered with this Mod Manager /
+	 * Client. Do not directly add to this ArrayList at any point after Runtime,
+	 * or after main initialization to prevent any Memory Leaks and or events
+	 * being called multiple times and other weird things.
+	 */
+	private ArrayList<Module> mods;
+	
+	/**
+	 * A ArrayList of enabled mods, please use
+	 * {@link ModManager#getEnabledMods()} instead of this because this will not
+	 * be updated all the time, unless {@link ModManager#getEnabledMods()} is
+	 * called. This is just used so that whenever you call
+	 * {@link ModManager#getEnabledMods()}, it doesn't initialize a whole new
+	 * ArrayList (just to increase performance).
+	 */
+	private ArrayList<Module> enabledMods;
 	
 	public ModManager()
 	{
 		this.mods = new ArrayList<Module>();
 	}
 	
+	/**
+	 * Registers the specified module by adding it to the
+	 * {@link ModManager#mods} ArrayList.
+	 */
 	public void registerMod(Module mod)
 	{
 		this.mods.add(mod);
 	}
 	
+	/**
+	 * Returns an ArrayList of all of the currently enabled Modules.
+	 */
+	public ArrayList<Module> getEnabledMods()
+	{
+		for (Module mod : mods)
+		{
+			if (mod.isEnabled() && !mods.contains(mod))
+			{
+				mods.add(mod);
+			}
+			else if (mods.contains(mod))
+			{
+				mods.remove(mod);
+			}
+		}
+		
+		return this.enabledMods;
+	}
+	
+	/**
+	 * Returns a Module with the specified name / identifier.
+	 * <p>
+	 * Will return {@code null} if no Module is found with the specified name.
+	 */
 	public Module getModByName(String name)
 	{
 		for (Module mod : mods)
--- me/newgarbo/garbobase/module/mods/Module.java
+++ me/newgarbo/garbobase/module/mods/Module.java
@@ -6,37 +6,81 @@ import me.newgarbo.garbobase.utils.Key;
 
 public abstract class Module
 {
+	/**
+	 * The string that will be displayed in the ArrayList when this Module is
+	 * enabled. Will be {@code null} if this module is not rendered in the
+	 * ArrayList.
+	 */
 	private String displayName;
+	
+	/**
+	 * The internal name / identifier of this module (has to be unique from any
+	 * other Modules so that there is a way to identify this Module as a
+	 * seperate one from others.
+	 */
 	private String name;
+	
+	/**
+	 * Whether or not this Module is enabled, pretty self explanatory.
+	 */
 	private boolean isEnabled;
+	
+	/**
+	 * The Key instance which defines the Keybind for this Module, will be null
+	 * if this Module is not toggleable through Keybinds.
+	 */
 	private Key keybind;
+	
+	/**
+	 * The color that this module will be rendered as in the ArrayList. Will be
+	 * {@code -1} if this Module is not rendered in the ArrayList.
+	 */
 	private int color;
 	
+	/**
+	 * Returns the {@link Module#displayName}
+	 */
 	public String getDisplayName()
 	{
 		return this.displayName;
 	}
 	
+	/**
+	 * Returns the {@link Module#name}
+	 */
 	public String getName()
 	{
 		return this.name;
 	}
 	
+	/**
+	 * Returns {@link Module#isEnabled}
+	 */
 	public boolean isEnabled()
 	{
 		return this.isEnabled;
 	}
 	
+	/**
+	 * Returns the {@link Module#keybind}
+	 */
 	public Key getBind()
 	{
 		return this.keybind;
 	}
 	
+	/**
+	 * Returns the {@link Module#color}
+	 */
 	public int getColor()
 	{
 		return this.color;
 	}
 	
+	/**
+	 * Toggles this module, calls {@code onToggle()} and then calls
+	 * {@code onEnable()} or {@code onDisable()} accordingly.
+	 */
 	public void toggle()
 	{
 		this.isEnabled = !this.isEnabled;
