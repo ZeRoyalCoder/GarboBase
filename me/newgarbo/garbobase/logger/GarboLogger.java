7b360433305f790356df5d6ae82fedbb5e47a0bb
ZeRoyalCoder <bernhardkiv@hotmail.com>
2014-12-18 21:00:10 +0000

    I dun goofed, and I logged the format only .-.

--- me/newgarbo/garbobase/logger/GarboLogger.java
+++ me/newgarbo/garbobase/logger/GarboLogger.java
@@ -1,5 +1,7 @@
 package me.newgarbo.garbobase.logger;
 
+import me.newgarbo.garbobase.utils.StringUtils;
+
 public class GarboLogger
 {
 	/**
@@ -7,6 +9,9 @@ public class GarboLogger
 	 */
 	public String clientName;
 	
+	/**
+	 * The format to use when logging messages.
+	 */
 	private static final String LOGGING_FORMAT = "[{0}]: {1}";
 	
 	public GarboLogger(String client)
@@ -14,8 +19,22 @@ public class GarboLogger
 		this.clientName = client;
 	}
 	
-	public void log(String n){
-		System.out.println(LOGGING_FORMAT);
+	/**
+	 * Logs a message to the console ({@code System.out.println}) using the
+	 * default prefix, which is {@code clientName}.
+	 */
+	public void log(String text)
+	{
+		System.out.println(StringUtils.format(LOGGING_FORMAT, clientName, text));
+	}
+	
+	/**
+	 * Logs a message to the console ({@code System.out.println}) using the
+	 * second specified String as the prefix.
+	 */
+	public void log(String text, String pre)
+	{
+		System.out.println(StringUtils.format(LOGGING_FORMAT, pre, text));
 	}
 	
 }
